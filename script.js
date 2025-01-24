d1aa5f78c2341af84b67c3eeeb2d520b420c8a1a
bke82024 <bke82024@gmail.com>
2025-01-24 13:04:41 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityinput");
+const card = document.querySelector(".card");
+const apiKey = "";
