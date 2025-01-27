411ba8a7e2e0b1b7e08af4f5001602b6a998b35a
spiderxiv <erikbirknes@hotmail.no>
2025-01-27 11:30:31 +0100

    start over

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityinput");
 const card = document.querySelector(".card");
-const apiKey = "";
+const apiKey = "91c2d2b9ec3a8bb96f293393c0a89a57";
